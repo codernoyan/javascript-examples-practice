19cfa4f5263106adc1840478ddde7d36adc4452e
codernoyan <coder.noyan@gmail.com>
2022-08-04 13:03:46 +0600

    objects

---
+++ object.js
@@ -0,0 +1,26 @@
+var student = {
+    id: 115,
+    name: 'Solaiman Khan',
+    class: 9,
+    marks: 98
+}
+
+var mobile = {
+    brand: 'Xiaomi',
+    price: 25000,
+    storage: '64GB',
+    camera: '64MP'
+}
+
+var myComputer = {
+    brand: 'acer',
+    price: 38000,
+    color: 'gray',
+    processor: "i3"
+}
+
+// console.log(myComputer);
+console.log(myComputer);
+myComputer.processor = 'i79';
+console.log(myComputer);
+console.log(mobile.brand);
